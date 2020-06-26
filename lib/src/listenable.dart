b1542f49f5d936769176167a649fb734bb0421b1
Chaoqian Xu <chaoranxu@gmail.com>
2020-06-26 19:11:30 +0800

    support modification when iteration

--- lib/src/listenable.dart
+++ lib/src/listenable.dart
@@ -1,12 +1,19 @@
 import 'package:maxwell_client/maxwell_client.dart';
+import 'package:tuple/tuple.dart';
 import './logger.dart';
 
 typedef OnEvent = void Function([dynamic result]);
 
 mixin Listenable {
   Map<Event, List<OnEvent>> _listeners = Map();
+  List<Tuple3> _pendingListeners = List();
+  bool _isInIteration = false;
 
   void addListener(Event event, OnEvent callback) {
+    if (this._isInIteration) {
+      this._pendingListeners.add(Tuple3(1, event, callback));
+      return;
+    }
     var callbacks = this._listeners[event];
     if (callbacks == null) {
       callbacks = [];
@@ -18,6 +25,10 @@ mixin Listenable {
   }
 
   void removeListener(Event event, OnEvent callback) {
+    if (this._isInIteration) {
+      this._pendingListeners.add(Tuple3(0, event, callback));
+      return;
+    }
     var callbacks = this._listeners[event];
     if (callbacks == null) {
       return;
@@ -34,16 +45,28 @@ mixin Listenable {
     if (callbacks == null) {
       return;
     }
-    callbacks.forEach((callback) {
-      try {
-        if (result != null) {
-          callback(result);
-        } else {
-          callback();
+    try {
+      this._isInIteration = true;
+      callbacks.forEach((callback) {
+        try {
+          if (result != null) {
+            callback(result);
+          } else {
+            callback();
+          }
+        } catch (e, s) {
+          logger.e('Failed to notify: reason: $e, stack: $s');
         }
-      } catch (e, s) {
-        logger.e('Failed to notify: reason: $e, stack: $s');
-      }
-    });
+      });
+    } finally {
+      this._isInIteration = false;
+      this._pendingListeners.forEach((tuple) {
+        if (tuple.item1 == 1) {
+          this.addListener(tuple.item2, tuple.item3);
+        } else if (tuple.item1 == 0) {
+          this.removeListener(tuple.item2, tuple.item3);
+        }
+      });
+    }
   }
 }
